2007f6c2fa0224a68537f3ef88e52f563405c083
renlong <renlong@hhb123.cn>
2022-04-12 11:39:46 +0800

    add zull-proxy API网关服务

--- feign-service/src/main/java/com/example/feignservice/FeignServiceApplication.java
+++ feign-service/src/main/java/com/example/feignservice/FeignServiceApplication.java
@@ -2,11 +2,13 @@ package com.example.feignservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @EnableDiscoveryClient
+@EnableCircuitBreaker
 @SpringBootApplication
 public class FeignServiceApplication {
 
