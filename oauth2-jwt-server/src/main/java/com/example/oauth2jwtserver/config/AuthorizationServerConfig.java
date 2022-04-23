b983b17056c45b7a38c9464cdd6061379b88a7fd
renlong <renlong@hhb123.cn>
2022-04-23 17:46:17 +0800

    add oauth2-client模块

--- oauth2-jwt-server/src/main/java/com/example/oauth2jwtserver/config/AuthorizationServerConfig.java
+++ oauth2-jwt-server/src/main/java/com/example/oauth2jwtserver/config/AuthorizationServerConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenEnhancer;
 import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
@@ -52,30 +53,49 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
      */
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
-        //配置JWT的内容增强器start
-        TokenEnhancerChain enhancerChain = new TokenEnhancerChain();
-        List<TokenEnhancer> delegates = new ArrayList<>();
-        delegates.add(jwtTokenEnhancer);
-        delegates.add(jwtAccessTokenConverter);
-        enhancerChain.setTokenEnhancers(delegates);
-        //配置JWT的内容增强器end
+//        //配置JWT的内容增强器start
+//        TokenEnhancerChain enhancerChain = new TokenEnhancerChain();
+//        List<TokenEnhancer> delegates = new ArrayList<>();
+//        delegates.add(jwtTokenEnhancer);
+//        delegates.add(jwtAccessTokenConverter);
+//        enhancerChain.setTokenEnhancers(delegates);
+//        //配置JWT的内容增强器end
         endpoints.authenticationManager(authenticationManager)
                 .userDetailsService(userService)
                 .tokenStore(tokenStore) //配置令牌存储策略
-                .accessTokenConverter(jwtAccessTokenConverter)   //指定令牌的存储策略为JWT
-                .tokenEnhancer(enhancerChain);  //配置JWT的内容增强器
+                .accessTokenConverter(jwtAccessTokenConverter);   //指定令牌的存储策略为JWT
+//                .tokenEnhancer(enhancerChain);  //配置JWT的内容增强器
     }
+
+//    @Override
+//    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+//        clients.inMemory()
+//                .withClient("admin")//配置client_id
+//                .secret(passwordEncoder.encode("admin123456"))//配置client_secret
+//                .accessTokenValiditySeconds(3600)//配置访问token的有效期
+//                .refreshTokenValiditySeconds(864000)//配置刷新token的有效期
+//                .redirectUris("http://www.baidu.com")//配置redirect_uri，用于授权成功后跳转
+//                .autoApprove(true) //自动授权配置
+//                .scopes("all")//配置申请的权限范围
+//                .authorizedGrantTypes("authorization_code", "password", "refresh_token");//配置grant_type，表示授权类型,添加授权模式
+//    }
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients.inMemory()
-                .withClient("admin")//配置client_id
-                .secret(passwordEncoder.encode("admin123456"))//配置client_secret
-                .accessTokenValiditySeconds(3600)//配置访问token的有效期
-                .refreshTokenValiditySeconds(864000)//配置刷新token的有效期
-                .redirectUris("http://www.baidu.com")//配置redirect_uri，用于授权成功后跳转
+                .withClient("admin")
+                .secret(passwordEncoder.encode("admin123456"))
+                .accessTokenValiditySeconds(3600)
+                .refreshTokenValiditySeconds(864000)
+//                .redirectUris("http://www.baidu.com")
+                .redirectUris("http://localhost:9501/login") //单点登录时配置
                 .autoApprove(true) //自动授权配置
-                .scopes("all")//配置申请的权限范围
-                .authorizedGrantTypes("authorization_code", "password", "refresh_token");//配置grant_type，表示授权类型,添加授权模式
+                .scopes("all")
+                .authorizedGrantTypes("authorization_code", "password", "refresh_token");
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) {
+        security.tokenKeyAccess("isAuthenticated()"); // 获取密钥需要身份认证，使用单点登录时必须配置
     }
 }
